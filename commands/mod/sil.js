24dada5aaa6bd1d18a0e7832f60ab2a3354344b3
Yakup Ünal <yakipunal977@gmail.com>
2024-10-02 22:06:47 +0300

    Added delete

---
+++ commands/mod/sil.js
@@ -0,0 +1,62 @@
+import {SlashCommandBuilder} from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("sil")
+        .setDescription("Belirtilen miktarda mesajı siler.")
+        .addIntegerOption(option =>
+             option
+             .setName("miktar").setDescription("Silinecek mesaj sayısı")
+             .setMinValue(1)
+             .setMaxValue(100)
+             .setRequired(true)),
+
+
+   async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+        const yetkiYokEmbed = new EmbedBuilder()
+         .setTitle("Yetkisiz Erişim")
+         .setDescription("Bu komutu kullanmak için gerekli yetkilere sahip olmalısınız.")
+         .setColor("Red")
+
+        return await interaction.reply({
+          embeds: [yetkiYokEmbed],
+          ephemeral: true,
+        });
+    }
+
+    const amount = interaction.options.getInteger("miktar");
+
+    try {
+        await interaction.channel.bulkDelete(amount);
+
+        const successEmbed = new EmbedBuilder()
+         .setTitle("Mesajlar Silindi")
+         .setDescription("Mesajlar başarıyla silindi.")
+         .setColor("Green")
+
+        await interaction.reply({
+          embeds: [successEmbed],
+          ephemeral: true,
+         });
+
+    } catch (error) {
+        const hataEmbed = new EmbedBuilder()
+          .setTitle("Bir hata oluştu")
+          .setDescription(`Bir hata oluştu:`)
+          .setColor("Red")
+
+        await interaction.reply({
+          embeds: [hataEmbed],
+          ephemeral: true,
+        });
+
+        console.error(
+            `Silme işlemi sırasında bir hata oluştu: ${error}`,
+        );
+    }
+
+
+
+   }
+}
